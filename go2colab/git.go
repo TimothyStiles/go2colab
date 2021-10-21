369ec43f4c509009691cb822179773059f09d723
Timothy Stiles <tim@stiles.io>
2021-10-20 21:44:43 -0700

    got basic directory reading working.

--- go2colab/git.go
+++ go2colab/git.go
@@ -2,6 +2,7 @@ package go2colab
 
 import (
 	"errors"
+	"io"
 	"net/url"
 	"sort"
 	"strings"
@@ -143,4 +144,47 @@ func getRepoUrlMeta(repo Repo) (Repo, error) {
 		repo.UseLatestReleaseTag = true
 	}
 	return repo, nil
+}
+
+func getBlobs(repo *git.Repository) ([]BlobInfo, error) {
+
+	// blob object types: https://github.com/go-git/go-git/blob/4ec1753b4e9324d455d3b55060020ce324e6ced2/plumbing/object.go#L42
+	// InvalidObject ObjectType = 0
+	// CommitObject  ObjectType = 1
+	// TreeObject    ObjectType = 2
+	// BlobObject    ObjectType = 3
+	// TagObject     ObjectType = 4
+
+	var blobDetails []BlobInfo
+	blobs, err := repo.BlobObjects()
+	if err != nil {
+		return nil, err
+	}
+	for {
+		blob, err := blobs.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+
+		var blobInfo BlobInfo
+		blobInfo.Hash = blob.Hash.String()
+		blobInfo.ObjectType = blob.Type()
+		blobInfo.Size = blob.Size
+		blobInfo.BlobReader, err = blob.Reader()
+		if err != nil {
+			return nil, err
+		}
+		blobDetails = append(blobDetails, blobInfo)
+	}
+	return blobDetails, nil
+}
+
+type BlobInfo struct {
+	BlobReader io.ReadCloser
+	Hash       string
+	ObjectType plumbing.ObjectType
+	Size       int64
 }
--- go2colab/go2colab.go
+++ go2colab/go2colab.go
@@ -2,10 +2,6 @@ package go2colab
 
 import (
 	"fmt"
-	"io"
-
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
 )
 
 func Go2Colab(urlString string) error {
@@ -39,53 +35,21 @@ func Go2Colab(urlString string) error {
 		return err
 	}
 	root := tree.Filesystem.Root()
+	files, err := tree.Filesystem.ReadDir(root)
+	if err != nil {
+		return err
+	}
+
+	var fileNames []string
+	for _, file := range files {
+		fileNames = append(fileNames, file.Name())
+	}
+
+	fmt.Println(fileNames)
 	treeStat, err := tree.Status()
 	if err != nil {
 		return err
 	}
 	fmt.Println(root, treeStat)
 	return nil
-}
-
-func getBlobs(repo *git.Repository) ([]BlobInfo, error) {
-
-	// blob object types: https://github.com/go-git/go-git/blob/4ec1753b4e9324d455d3b55060020ce324e6ced2/plumbing/object.go#L42
-	// InvalidObject ObjectType = 0
-	// CommitObject  ObjectType = 1
-	// TreeObject    ObjectType = 2
-	// BlobObject    ObjectType = 3
-	// TagObject     ObjectType = 4
-
-	var blobDetails []BlobInfo
-	blobs, err := repo.BlobObjects()
-	if err != nil {
-		return nil, err
-	}
-	for {
-		blob, err := blobs.Next()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return nil, err
-		}
-
-		var blobInfo BlobInfo
-		blobInfo.Hash = blob.Hash.String()
-		blobInfo.ObjectType = blob.Type()
-		blobInfo.Size = blob.Size
-		blobInfo.BlobReader, err = blob.Reader()
-		if err != nil {
-			return nil, err
-		}
-		blobDetails = append(blobDetails, blobInfo)
-	}
-	return blobDetails, nil
-}
-
-type BlobInfo struct {
-	BlobReader io.ReadCloser
-	Hash       string
-	ObjectType plumbing.ObjectType
-	Size       int64
 }
