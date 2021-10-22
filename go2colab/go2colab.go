e323bfe663adb5903694843d001bd5d45ee6a98f
Timothy Stiles <tim@stiles.io>
2021-10-21 19:28:27 -0700

    refactored for more clarity and modularity.

--- go2colab/go2colab.go
+++ go2colab/go2colab.go
@@ -1,11 +1,8 @@
 package go2colab
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"os"
-	"path/filepath"
 	"regexp"
 	"strings"
 
@@ -38,76 +35,117 @@ func Go2Colab(urlString string) error {
 		return err
 	}
 
-	tree, err := gitRepo.Worktree()
+	// get go version for docker container build
+	repo.GoVersion, err = getGoVersion(gitRepo)
 	if err != nil {
 		return err
+	}
+
+	// retrieve all example tests with keyword "tutorial" in them as Notebooks
+	notebooks, err := getTutorials(gitRepo)
+	if err != nil {
+		return err
+	}
+	fmt.Println(notebooks)
+	// file, _ := json.MarshalIndent(notebook, "", " ")
+	// base := filepath.Base(notebook.RepoPath)
+	// filename := filepath.Join(tmpDataDir, base)
+	// _ = ioutil.WriteFile(filename, file, 0644)
+
+	return nil
+}
+
+func getGoVersion(gitRepo *git.Repository) (string, error) {
+	var version string
+	tree, err := gitRepo.Worktree()
+	if err != nil {
+		return version, err
 	}
 
 	// grep Go Version
 	goVersionRegex := regexp.MustCompile("go [\\d].*")
 	grepResults, err := grepWorkTree(tree, *goVersionRegex)
 	if err != nil {
-		return err
+		return version, err
 	}
 
 	// iterate over grep results and get the version when we find the first match for go.mod
 	for _, result := range grepResults {
 		if result.FileName == "go.mod" {
-			version := strings.Split(result.Content, " ")[1]
-			repo.GoVersion = version
+			version = strings.Split(result.Content, " ")[1]
 			break
 		}
 	}
+	return version, nil
+}
 
-	// grep Tutorial examples
-	tutorialRegex := regexp.MustCompile("Example_basic")
-	grepResults, err = grepWorkTree(tree, *tutorialRegex)
-	if err != nil {
-		return err
-	}
-
-	tmpDataDir, err := ioutil.TempDir(".", "data-*")
-	if err != nil {
-		fmt.Println(err.Error())
-	}
-	defer os.RemoveAll(tmpDataDir)
-
-	//  iterate over example files, check filename, and store them as billy.File slice
+func getTutorials(gitRepo *git.Repository) ([]Notebook, error) {
 	var notebooks []Notebook
+
+	tree, err := gitRepo.Worktree()
+	if err != nil {
+		return notebooks, err
+	}
+
+	// grep Tutorial examples for "Tutorial" keyword.
+	// Returns slice of git.GrepResult structs which contain filenames
+	// containing our keyword.
+	tutorialRegex := regexp.MustCompile("Example_basic")
+	grepResults, err := grepWorkTree(tree, *tutorialRegex)
+	if err != nil {
+		return notebooks, err
+	}
+
+	// iterate through all grep results. Filter files that don't have
+	// "example" and convert the rest to Notebooks that can be written out.
 	for _, result := range grepResults {
+		// go2colab only looks at example files.
 		if strings.Contains(result.FileName, "example") {
-			notebook, err := grep2Notebook(tree, result)
+
+			notebook, err := createNotebook(result.FileName, tree)
 			if err != nil {
-				return err
+				return notebooks, err
 			}
-			file, _ := json.MarshalIndent(notebook, "", " ")
-			base := filepath.Base(notebook.RepoPath)
-			filename := filepath.Join(tmpDataDir, base)
-			_ = ioutil.WriteFile(filename, file, 0644)
+			notebook.RepoPath = result.FileName
+
+			// append to our notebooks to be returned.
 			notebooks = append(notebooks, notebook)
 		}
 	}
 
-	return nil
+	return notebooks, nil
 }
 
-func grep2Notebook(tree *git.Worktree, grepResult git.GrepResult) (Notebook, error) {
+func createNotebook(path string, tree *git.Worktree) (Notebook, error) {
+
+	// intialize notebook and the jupyter "cell" where we'll be storing the example code.
+	// notebooks can have many source cells but here we're just using one
+	// since we're more concerned with have a repl for MVP.
 	var notebook Notebook
 	var sourceCell Cell
-	sourceCell.CellType = "code"
-	src, err := tree.Filesystem.Open(grepResult.FileName)
+	sourceCell.CellType = "code" // Need to let jupyter know that this cell should be rendered as code.
+
+	// this is us retrieving the file from the in memory filesystem that represents our git tree.
+	src, err := tree.Filesystem.Open(path)
 	if err != nil {
 		return notebook, err
 	}
+
+	// close the file after this function piece of control flow ends
 	defer src.Close()
 
+	// read source file contents into bytes
 	srcBytes, err := ioutil.ReadAll(src)
 	if err != nil {
 		return notebook, err
 	}
+	// convert source bytes to string and add to our code cell.
 	srcString := string(srcBytes)
 	sourceCell.Source = srcString
+
+	// add the source cell to our notebook.
+	// Now notebook can be rendered as jupyter notebook when written out.
 	notebook.Cells = append(notebook.Cells, sourceCell)
-	notebook.RepoPath = grepResult.FileName
+
 	return notebook, nil
 }
